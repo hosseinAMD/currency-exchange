d0098586c29ee52007f7102850d37221c88a99dd
Hossien Ahmadi <hossein.ahmadi@snapp.cab>
2021-07-24 00:17:13 +0430

    feat: i18n basic implementation done

---
+++ src/i18n/en.ts
@@ -0,0 +1,6 @@
+export const enStrings: Record<string, string> = {
+  currency: "Currency",
+  exchange: "Exchange",
+  currencyConvertor: "currency convertor",
+  viewConversionHistory: "view conversion history",
+};
