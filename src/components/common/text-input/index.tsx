ba022d46b8f8d83bd79e21be0244605e70b27ee0
Hossien Ahmadi <hossein.ahmadi@snapp.cab>
2021-07-24 00:46:46 +0430

    feat: text input basic design done

---
+++ src/components/common/text-input/index.tsx
@@ -0,0 +1,25 @@
+import { ChangeEvent } from "react";
+import FormElement from "../form-element";
+import "./text-input.scss";
+
+export interface TextInputProps {
+  name: string;
+  label: string;
+  value?: string;
+  onChange: (e: ChangeEvent<HTMLInputElement>) => void;
+}
+
+const TextInput: React.FC<TextInputProps> = ({
+  name,
+  label,
+  value,
+  onChange,
+}) => {
+  return (
+    <FormElement label={label}>
+      <input name={name} value={value} onChange={onChange} className="input" />
+    </FormElement>
+  );
+};
+
+export default TextInput;
