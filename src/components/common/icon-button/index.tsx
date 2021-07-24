6cfbcd5cf2cc7eabb62472ed523b26a8d692a1f7
Hossien Ahmadi <hossein.ahmadi@snapp.cab>
2021-07-24 12:31:29 +0430

    feat: icon-button implemented

---
+++ src/components/common/icon-button/index.tsx
@@ -0,0 +1,16 @@
+import "./icon-button.scss";
+
+export interface IconButtonProps {
+  icon: string;
+  onClick: () => void;
+}
+
+const IconButton: React.FC<IconButtonProps> = ({ icon, onClick }) => {
+  return (
+    <button className="icon-button" onClick={onClick}>
+      <span className="material-icons">{icon}</span>
+    </button>
+  );
+};
+
+export default IconButton;
--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,5 +1,12 @@
+import IconButton from "components/common/icon-button";
+
 const Home: React.FC = () => {
-  return <div>Convertor</div>;
+  return (
+    <div>
+      <p>Convertor</p>
+      <IconButton icon="compare_arrows" onClick={() => {}} />
+    </div>
+  );
 };
 
 export default Home;
