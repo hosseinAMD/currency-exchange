08f220d9852ef320fc70141b8071ecf830407cb3
Hossien Ahmadi <hossein.ahmadi@snapp.cab>
2021-07-25 15:59:42 +0430

    fix: bugs fixed

--- src/utils/calculator.ts
+++ src/utils/calculator.ts
@@ -1,6 +1,14 @@
-export const getAvg = (array: Array<number>) =>
-  array.reduce((a, b) => Number(a) + Number(b)) / array.length;
+export const getAvg = (array: Array<number>) => {
+  if (array.length === 0) return 0;
+  return array.reduce((a, b) => Number(a) + Number(b)) / array.length;
+};
 
-export const getMin = (array: Array<number>) => Math.min(...array);
+export const getMin = (array: Array<number>) => {
+  if (array.length === 0) return 0;
+  return Math.min(...array);
+};
 
-export const getMax = (array: Array<number>) => Math.max(...array);
+export const getMax = (array: Array<number>) => {
+  if (array.length === 0) return 0;
+  return Math.max(...array);
+};
