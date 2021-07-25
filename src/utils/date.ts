843d2f0e59efdea964df95ae3527456f46cc2e61
Hossien Ahmadi <hossein.ahmadi@snapp.cab>
2021-07-25 16:21:23 +0430

    fix: cloning date to prevent object mutation

--- src/utils/date.ts
+++ src/utils/date.ts
@@ -1,12 +1,12 @@
 export const minusDays = (date: Date, count: number) => {
-  let result = date;
+  let result = new Date(date);
   result.setDate(result.getDate() - count);
   return result;
 };
 
 export const formatDate = (date: Date) => {
   const day = date.getDate().toString().padStart(2, "0");
-  const month = date.getMonth().toString().padStart(2, "0");
+  const month = (date.getMonth() + 1).toString().padStart(2, "0");
   const year = date.getFullYear();
   return `${day}/${month}/${year}`;
 };
@@ -14,7 +14,7 @@ export const formatDate = (date: Date) => {
 export const formatDateTime = (date: Date | string) => {
   const dateTime = typeof date === "string" ? new Date(date) : date;
   const dateString = formatDate(dateTime);
-  const hours = dateTime.getHours();
-  const minutes = dateTime.getMinutes();
+  const hours = dateTime.getHours().toString().padStart(2, "0");
+  const minutes = dateTime.getMinutes().toString().padStart(2, "0");
   return `${dateString}@ ${hours}:${minutes}`;
 };
